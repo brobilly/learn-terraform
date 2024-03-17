4ceeeb291d8e83e71e120af9155715d942104746
brobill <115522089+brobilly@users.noreply.github.com>
2024-03-17 23:07:42 +0700

    Update db.tf
    fixed

--- db.tf
+++ db.tf
@@ -1,11 +1,15 @@
 resource "aws_db_instance" "terra_db" {
-  allocated_storage    = 4
-  db_name              = "terra_db"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  username             = "admin"
-  password             = "admin"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
+  allocated_storage      = 10
+  storage_type           = "gp2"
+  db_name                = "terra_db"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = "password"
+  db_subnet_group_name   = "db_subnet"
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  parameter_group_name   = "default.mysql5.7"
+  skip_final_snapshot    = true
 }
+
